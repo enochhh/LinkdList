5a1ea3721eecb79543233b8c2588c091956b166f
enoch <enochharnantoo@gmail.com>
2022-01-05 23:06:37 -0800

    Added input and render list functionality, made list items links

---
+++ index.js
@@ -0,0 +1,25 @@
+let myLeads = [];
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value);
+    console.log(myLeads);
+    inputEl.value = "";
+    renderLeads();
+})
+function renderLeads() {
+    let listItems = "";
+    for (i = 0; i < myLeads.length; i++) {
+        listItems += `
+        <li>
+            <a href=${myLeads[i]} target='_blank' rel='noreferrer noopener'>
+                ${myLeads[i]}
+            </a>
+        </li>`;
+    }
+    ulEl.innerHTML = listItems;
+}
+
+
